5b07fefd757a03b722d7198a1b942d7bc22639e1
Andrew Watkins <andreww@gmail.com>
2014-05-02 10:35:53 -0400

    remove a lot of extraneous files

--- app/models/exportable_csv.rb
+++ app/models/exportable_csv.rb
@@ -2,10 +2,16 @@ module Exportable
   class ExportableCSV
     attr_reader :num_rows
 
-    def initialize(filename, headers, col_sep = "\t")
+    def initialize(filename, headers, col_sep = "\t", &block)
       @file = File.new(filename, "wb:UTF-16")
       @num_rows = 0
       @csv = CSV.new(@file, col_sep: col_sep, quote_char: col_sep, headers: headers, write_headers: true)
+
+      if block_given?
+        yield(self)
+        close
+      end
+      self
     end
 
     def close
--- config/routes.rb
+++
@@ -1,2 +0,0 @@
-Exportable::Engine.routes.draw do
-end
